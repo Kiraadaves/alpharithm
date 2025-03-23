72e2767f525dc74bfc7eb41db33ee3f4a1ad3af6
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2025-03-24 00:20:24 +0100

    adjust nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
         <nav
           role="navigation"
           aria-label="Main navigation"
-          className="hidden lg:flex items-center justify-end gap-8 pr-5 w-[45%]"
+          className="hidden lg:flex items-center justify-end gap-8 pr-5"
           data-aos="fade-down"
         >
           {navItems.map((item) => (
